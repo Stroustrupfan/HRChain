2f1c3390107675bb371a7d74e54b41ef666b112e
Stroustrupfan <81951140+Stroustrupfan@users.noreply.github.com>
2023-11-07 13:16:15 +0800

    Improved the html

--- src/js/app.js
+++ src/js/app.js
@@ -11,8 +11,27 @@ document.getElementById('hrChainForm').addEventListener('submit', function(e) {
   var hash = document.getElementById('hash').value;
   var signature = document.getElementById('signature').value;
 
-  // Here you can call the functions of your smart contract using web3.js
-  // For example:
-  // var contract = new web3.eth.Contract(abi, contractAddress);
-  // contract.methods.register_client(name, userID, info, docCount, stageAcc, docInfo, timestamp, hash, signature).send({from: '0xYourAccount'});
+  // Check if Web3 has been injected by the browser
+  if (typeof window.ethereum !== 'undefined') {
+    var Web3 = require('web3');
+    var web3 = new Web3(window.ethereum);
+    var contractAddress = ""; // replace with your contract address
+    var contract = new web3.eth.Contract(abi, contractAddress);
+
+    contract.methods.register_client(name, userID, info, docCount, stageAcc, docInfo, timestamp, hash, signature)
+      .send({from: '0xYourAccount'}) // replace with your account address
+      .on('receipt', function(receipt){
+        console.log(receipt);
+      })
+      .on('confirmation', function(confirmationNumber, receipt){
+        console.log('Confirmation number: ', confirmationNumber);
+        console.log('Receipt: ', receipt);
+      })
+      .on('error', function(error, receipt) {
+        console.log('Error: ', error);
+        console.log('Receipt: ', receipt);
+      });
+  } else {
+    console.log('Web3 is not detected. Please install MetaMask or another web3 provider.');
+  }
 });
--- test/TestHRChain.sol
+++ test/TestHRChain.sol
@@ -1,32 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import "../contracts/HRChain.sol";
+
 contract TestHRChain {
-    // The address of the adoption contract to be tested
-    Adoption adoption = Adoption(DeployedAddresses.Adoption());
-    // The id of the pet that will be used for testing
-    uint expectedPetId = 8;
-    // The expected owner of adopted pet is this contract
-    address expectedAdopter = address(this);
+    HRChain hrChain = HRChain(DeployedAddresses.HRChain());
 
-    // Testing the adopt() function
-    function testUserCanAdoptPet() public {
-        uint returnedId = adoption.adopt(expectedPetId);
-        Assert.equal(returnedId, expectedPetId, "Adoption of the expected pet should match what is returned.");
+    // The id of the user that will be used for testing
+    uint expectedUserId = 1;
+
+    // The expected owner of the user is this contract
+    address expectedOwner = address(this);
+
+    // Testing the addUser() function
+    function testUserCanAddUser() public {
+        uint returnedId = hrChain.addUser(expectedUserId);
+        Assert.equal(returnedId, expectedUserId, "Adding of the expected user should match what is returned.");
     }
 
-    // Testing retrieval of a single pet's owner
-    function testGetAdopterAddressByPetId() public {
-        address adopter = adoption.adopters(expectedPetId);
-        Assert.equal(adopter, expectedAdopter, "Owner of the expected pet should be this contract");
+    // Testing retrieval of a single user's owner
+    function testGetOwnerAddressByUserId() public {
+        address owner = hrChain.owners(expectedUserId);
+        Assert.equal(owner, expectedOwner, "Owner of the expected user should be this contract");
     }
 
-    // Testing retrieval of all pet owners
-    function testGetAdopterAddressByPetIdInArray() public {
-        // Store adopters in memory rather than contract's storage
-        address[16] memory adopters = adoption.getAdopters();
-        Assert.equal(adopters[expectedPetId], expectedAdopter, "Owner of the expected pet should be this contract");
+    // Testing retrieval of all user owners
+    function testGetOwnerAddressByUserIdInArray() public {
+        // Store owners in memory rather than contract's storage
+        address[] memory owners = hrChain.getOwners();
+        Assert.equal(owners[expectedUserId], expectedOwner, "Owner of the expected user should be this contract");
     }
-
-
 }
